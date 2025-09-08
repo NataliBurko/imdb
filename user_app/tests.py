61b66f496bfe32f7ae73a5f864188c97346b5e87
Natali Burko <nataliia.burko.dev@gmail.com>
2025-09-08 15:39:00 +0300

    added tests + requirements.txt

--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+        data ={
+            "username": "testcase",
+            "email": "testcase@test.com",
+            "password": "testcase"
+        }
+        response = self.client.post(reverse('register'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="example",
+                                        password="example123")
+        
+    def test_login(self):
+        data = {
+            "username": "example",
+            "password": "example123"
+
+        }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        login_resp = self.client.post(reverse('login'), {
+            "username": "example",
+            "password": "example123"
+        })
+        token = login_resp.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='Token '+ token)
+        response = self.client.post(reverse('logout'))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
